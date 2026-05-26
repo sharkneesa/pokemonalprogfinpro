6d365ca1aa55e6b273181e61505b965c613e2d57
sharkneesa <shanniisaa@gmail.com>
2026-05-27 01:52:25 +0700

    added pokemon data

---
+++ constants/data.h
@@ -0,0 +1,15 @@
+#ifndef DATA_POKE
+#define DATA_POKE
+
+#include <vector>
+#include "models/structs.h"
+
+using namespace std;
+
+extern vector<Pokemon>pokemons;
+extern vector<Trainer>trainers;
+extern vector<Location>locations;
+extern vector<Routes>routes;
+
+
+#endif
