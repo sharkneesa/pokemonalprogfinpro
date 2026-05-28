8fa87a34c11944ec1c8a65c36332743304243018
sharkneesa <shanniisaa@gmail.com>
2026-05-29 00:55:07 +0700

    added case 2 for switch case main.cpp

--- main.cpp
+++ main.cpp
@@ -22,58 +22,64 @@ int main() {
   
   do{
 
-  cout << "Select Action: ";
-  cin >> choice;
-  cout << endl;
+    cout << "Select Action: ";
+    cin >> choice;
+    cout << endl;
 
-  switch(choice){
+    switch(choice){
 
-    case 1:
+      case 1:
 
-    break;
+      break;
 
-    case 2:
+      case 2:
 
-      int choice2;
-      pokedexMenuShow();
-      cout << "Select Action: ";
-      cin >> choice2;
-      cout << endl;
-      if(choice2==1){
+        int choice2;
+
+        do{
+
+          pokedexMenuShow();
+          cout << "Select Action: ";
+          cin >> choice2;
+          cout << endl;
+          if(choice2==1){
       
-        string query;
-        cout << "Which Pokemon Would You Like to Search For: \n";
-        cin >> query;
-        searchByName(pokemons, query);
-      }
-      else if(choice2==2){
-        sortAndDisplayByIndex(pokemons);
-      }
+            string query;
+            cout << "Which Pokemon Would You Like to Search For: \n";
+            cin >> query;
+            searchByName(pokemons, query);
+          }
+          else if(choice2==2){
+            sortAndDisplayByIndex(pokemons);
+          }
 
-      else if(choice2==3){
-        groupAndDisplayByType(pokemons);
-      }
+          else if(choice2==3){
+            groupAndDisplayByType(pokemons);
+          }
 
-      else{
-        cout << "Invalid Choice Try Again!\n";
-      }
+          else{
+            cout << "Invalid Choice Try Again!\n";
+          }
 
-    break;
-
-    case 3:
-
-    break;
-
-    case 4:
-
-    break;
-
-    case 5:
-
-    break;
+        }while (choice2!=1&&choice2!=2&&choice2!=3);
 
 
-  }
+      break;
+
+      case 3:
+
+      break;
+
+      case 4:
+
+      break;
+
+      case 5:
+
+      break;
+
+
+    }
 
   }while(choice!=6);
   
